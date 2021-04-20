22e394a78612264f27ef3febf644fc44fd8d90d9
carlos738 <carlosasl135@hotmail.com>
2021-04-20 16:41:09 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PROJECT_SERIES_CARLOS_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
