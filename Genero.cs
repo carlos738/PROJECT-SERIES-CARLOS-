f768ccd328b0fb9821cb567567831ea5fd57d8e4
carlos738 <carlosasl135@hotmail.com>
2021-04-20 16:44:52 -0300

    Add files via upload

---
+++ Genero.cs
@@ -0,0 +1,18 @@
+namespace PROJECT_SERIES_CARLOS_.ENUM
+{
+    public enum Genero
+    {
+       Acao  = 1,
+       Aventura = 2,
+       Documentario = 3,
+       Drama = 5,
+       Espionagem = 6,
+       Faroeste = 7,
+       Fantasia = 8,
+       Ficcao_Cientifica = 9,
+       Musical = 10,
+       Romance = 11, 
+       Terror = 12
+       Comedia = 13
+    }  
+}
